4e9f9b642e8e3d0f8e63848f55144c92e5a5ed8d
MatteoZnz <mtt.zinzani@gmail.com>
2023-09-07 13:57:10 +0200

    braitenberg completed

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,9 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    res[80:480, 0:260] = 1
+    #res[320:400, 260:320] = 5
+    #res[300:, 200:] = 1
     # ---
     return res
 
@@ -17,6 +18,7 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    res[80:480, 380:] = -1
+    #res[320:400, 320:] = -5
     # ---
     return res
--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([171, 140, 100])
-upper_hsv = np.array([179, 200, 255])
+lower_hsv = np.array([0, 17, 91])
+upper_hsv = np.array([24, 194, 255])
 
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
